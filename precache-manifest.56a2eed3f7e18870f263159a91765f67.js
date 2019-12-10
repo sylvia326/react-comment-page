eed26ed947d091df45a06d9c50b7d1a8af60f166
sylvia326 <hxiaowei@uwo.ca>
2019-12-10 14:39:02 -0500

    Updates

---
+++ precache-manifest.56a2eed3f7e18870f263159a91765f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f067160a7ca63da54614489aaf99f7a5",
+    "url": "/react-comment-page/index.html"
+  },
+  {
+    "revision": "9c68b1272de8ec8b1757",
+    "url": "/react-comment-page/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "95880d30ac3cb2947bff",
+    "url": "/react-comment-page/static/css/main.24bb83aa.chunk.css"
+  },
+  {
+    "revision": "9c68b1272de8ec8b1757",
+    "url": "/react-comment-page/static/js/2.2a581162.chunk.js"
+  },
+  {
+    "revision": "95880d30ac3cb2947bff",
+    "url": "/react-comment-page/static/js/main.79756b60.chunk.js"
+  },
+  {
+    "revision": "b01ef45910e482352031",
+    "url": "/react-comment-page/static/js/runtime-main.a00e45c4.js"
+  }
+]);
